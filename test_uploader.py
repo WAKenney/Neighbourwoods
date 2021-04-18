cd3270d1257a136279369e74ba1d993302468476
Andy Kenney <a.kenney@utoronto.ca>
2021-04-18 18:32:56 -0400

    removed try test

--- test_uploader.py
+++ test_uploader.py
@@ -2,17 +2,27 @@ import pandas as pd
 import io
 import streamlit as st
 
+# try:
+#     myfile = st.file_uploader('Get the tree data file from your hard drive')
+
+#     with st.spinner('Please wait while your file is uploaded...'):
+#         if myfile is not None:
+#             # df = pd.read_csv(myfile,encoding='cp1252')
+#             text_io = io.TextIOWrapper(myfile)
+#             df = pd.read_csv(text_io)
+#             st.write(df)
+            
+# except:
+#     st.error("Oops, this isn't a valid file")
+
 myfile = st.file_uploader('Get the tree data file from your hard drive')
 
-try:
-    with st.spinner('Please wait while your file is uploaded...'):
-        if myfile is not None:
-            # df = pd.read_csv(myfile,encoding='cp1252')
-            text_io = io.TextIOWrapper(myfile)
-            df = pd.read_csv(text_io)
-
-            st.write(df)
-            
-except:
-    st.error("Oops, this isn't a valid file")
+with st.spinner('Please wait while your file is uploaded...'):
+    if myfile is not None:
+        # df = pd.read_csv(myfile,encoding='cp1252')
+        text_io = io.TextIOWrapper(myfile)
+        df = pd.read_csv(text_io)
+        st.write(df)
+        
+# st.error("Oops, this isn't a valid file")
     
