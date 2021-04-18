596c3c4d116358b433b2d30606e2454642268d75
Andy Kenney <a.kenney@utoronto.ca>
2021-04-18 16:37:13 -0400

    various changes to file uploader test

--- test_uploader.py
+++ test_uploader.py
@@ -1,10 +1,17 @@
 import pandas as pd
+import io
 import streamlit as st
 
-# st.set_option('deprecation.showfileUploaderEncoding', False)
+myfile = st.file_uploader('Get the tree data file')
 
-myfile = st.file_uploader('Get the data file')
-if myfile is not None:
-    df = pd.read_csv(myfile,encoding='cp1252')
-    # df = pd.read_csv(myfile)
-    st.write(df)
+try:
+    with st.spinner('Please wait while your file is uploaded...'):
+        if myfile is not None:
+            # df = pd.read_csv(myfile,encoding='cp1252')
+            text_io = io.TextIOWrapper(myfile)
+            df = pd.read_csv(text_io)
+            st.write(df)
+            
+except:
+    st.error("Oops, this isn't a valid file")
+    
