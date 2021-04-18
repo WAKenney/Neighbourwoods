cc4b4655070b8bec04b1fedc5e8d6cc99d922346
Andy Kenney <a.kenney@utoronto.ca>
2021-04-18 18:22:42 -0400

    more updates

--- test_uploader.py
+++ test_uploader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import io
 import streamlit as st
 
-myfile = st.file_uploader('Get the tree data file')
+myfile = st.file_uploader('Get the tree data file from your hard drive')
 
 try:
     with st.spinner('Please wait while your file is uploaded...'):
@@ -10,6 +10,7 @@ try:
             # df = pd.read_csv(myfile,encoding='cp1252')
             text_io = io.TextIOWrapper(myfile)
             df = pd.read_csv(text_io)
+
             st.write(df)
             
 except:
