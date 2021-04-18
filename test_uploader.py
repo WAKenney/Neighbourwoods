888473c543f9935d22194c9634047940a3ac17bb
Andy Kenney <a.kenney@utoronto.ca>
2021-04-18 18:39:34 -0400

    added encoding again

--- test_uploader.py
+++ test_uploader.py
@@ -19,9 +19,10 @@ myfile = st.file_uploader('Get the tree data file from your hard drive')
 
 with st.spinner('Please wait while your file is uploaded...'):
     if myfile is not None:
-        # df = pd.read_csv(myfile,encoding='cp1252')
-        text_io = io.TextIOWrapper(myfile)
-        df = pd.read_csv(text_io)
+        df = pd.read_csv(myfile,encoding='cp1252')
+        
+        # text_io = io.TextIOWrapper(myfile)
+        # df = pd.read_csv(text_io)
         st.write(df)
         
 # st.error("Oops, this isn't a valid file")
